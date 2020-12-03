ad541dfef0aa2b109dbd806c0f13d983bcc05511
yuiokjgft <73433100+yuiokjgft@users.noreply.github.com>
2020-12-03 19:58:41 +0700

    Update ban_user.py

--- bot/plugins/ban_user.py
+++ bot/plugins/ban_user.py
@@ -24,34 +24,34 @@ from pyrogram.types import (
 )
 from bot import (
     AUTH_CHANNEL,
-    BAN_COMMAND,
     BOT_WS_BLOCKED_BY_USER,
     COMMM_AND_PRE_FIX,
-    IS_BLACK_LIST_ED_MESSAGE_TEXT,
-    REASON_DE_LIMIT_ER
+    IS_UN_BANED_MESSAGE_TEXT,
+    REASON_DE_LIMIT_ER,
+    UN_BAN_COMMAND
 )
 from bot.hf.fic import vhkzuoi_repliz_handler
 from bot.hf.flifi import uszkhvis_chats_ahndler
 from bot.hf.stuf import get_tle_mof_t
 from bot.sql.users_sql import get_user_id
-from bot.sql.blacklist_sql import add_user_to_bl
+from bot.sql.blacklist_sql import rem_user_from_bl
 
 
 @Client.on_message(
-    filters.command(BAN_COMMAND, COMMM_AND_PRE_FIX) &
+    filters.command(UN_BAN_COMMAND, COMMM_AND_PRE_FIX) &
     uszkhvis_chats_ahndler([AUTH_CHANNEL]) &
     vhkzuoi_repliz_handler
 )
-async def ban_command(client: Client, message: Message):
+async def un_ban_command(client: Client, message: Message):
     user_id, reply_message_id = get_user_id(
         message.reply_to_message.message_id
     )
-    _, ban_reason = get_tle_mof_t(message.text)
-    add_user_to_bl(user_id, ban_reason)
-    black_list_message = IS_BLACK_LIST_ED_MESSAGE_TEXT.format(
-        reason=ban_reason
+    _, unban_reason = get_tle_mof_t(message.text)
+    rem_user_from_bl(user_id)
+    black_list_message = IS_UN_BANED_MESSAGE_TEXT.format(
+        reason=unban_reason
     )
-    if not ban_reason:
+    if not unban_reason:
         black_list_message = black_list_message.split(
             REASON_DE_LIMIT_ER
         )[0]
@@ -71,6 +71,5 @@ async def ban_command(client: Client, message: Message):
     await message.reply_text(
         f"<a href='tg://user?id={user_id}'>"
         "user"
-        "</a> <b>banned</b> <i>forever</i>."
-        "<u>Reason</u>: <code>{reason}</code>"
+        "</a> <b>unbanned</b>."
     )
